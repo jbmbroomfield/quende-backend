15a6aa0281d87e6fb1d5c716ae7f961b487afdd8
jim <jbmbroomfield@gmail.com>
2021-10-22 21:26:32 +1100

    Implemented login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
+    skip_before_action :authorized, only: [:create]
+
     def create
-        puts [
-            '--------------',
-            user_params,
-            '------------'
-        ]
         user = User.create(user_params)
         if user.valid?
+            @token = encode_token(user_id: user.id)
             render json: { user: UserSerializer.new(user) }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
