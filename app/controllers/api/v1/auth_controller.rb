1f367cd4e0e24f3db632b02583796849c08a63c3
jim <jbmbroomfield@gmail.com>
2021-10-23 12:19:51 +1100

    Removed require_login skip in AuthController

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::AuthController < ApplicationController
 
-    skip_before_action :require_login, only: [:create]
-
     def create
         user = User.find_by(username: user_login_params[:username])
         if user && user.password_authenticate(user_login_params[:password_authentication_attributes][:password])
