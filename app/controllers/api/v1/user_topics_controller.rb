d9598e34df86e09f6d824429f3c1b13342a28b9c
jim <jbmbroomfield@gmail.com>
2021-10-30 20:35:00 +1100

    user_topics

--- app/controllers/api/v1/user_topics_controller.rb
+++ app/controllers/api/v1/user_topics_controller.rb
@@ -7,21 +7,24 @@ class Api::V1::UserTopicsController < ApplicationController
     end
 
     def subscribe
-        user_topic = get_user_topic
-        user_topic.subscribed = true
-        save_and_render(user_topic)
+        sub_or_unsub(true)
     end
 
     def unsubscribe
-        user_topic = get_user_topic
-        user_topic.subscribed = false
-        save_and_render(user_topic)
+        sub_or_unsub(false)
     end
 
     private
 
     def get_user_topic
         UserTopic.find_or_create_by(user: current_user, topic_id: params[:topic_id])
+    end
+
+    def sub_or_unsub(value)
+        user_topic = get_user_topic
+        user_topic.subscribed = value
+        user_topic.save
+        render_object(user_topic)
     end
 
 end
--- spec/requests/api/v1/user_topics_spec.rb
+++ spec/requests/api/v1/user_topics_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 user_topic1_attributes = {
+  user_id: 1,
   topic_id: 1,
   subscribed: false
+}
+user_topic1_attributes_subscribed = {
+  user_id: 1,
+  topic_id: 1,
+  subscribed: true
 }
 
 RSpec.describe "Api::V1::UserTopics", type: :request do
@@ -15,6 +21,31 @@ RSpec.describe "Api::V1::UserTopics", type: :request do
       get "/api/v1/user_topics/1", headers: headers
       expect(response).to have_http_status(:success)
       expect(data[:attributes]).to eq(user_topic1_attributes)
+    end
+  end
+
+  describe "POST subscribe" do
+    it "returns http success and subscribes" do
+      headers = {
+        "Authorization": user1_auth
+      }
+      post "/api/v1/user_topics/1/subscribe", headers: headers
+      expect(response).to have_http_status(:success)
+      expect(data[:attributes]).to eq(user_topic1_attributes_subscribed)
+      expect(UserTopic.first.subscribed).to eq(true)
+    end
+  end
+
+  describe "POST unsubscribe" do
+    it "returns http success and unsubscribes" do
+      headers = {
+        "Authorization": user1_auth
+      }
+      user_topic = UserTopic.create(user_id: 1, topic_id: 1, subscribed: true)
+      post "/api/v1/user_topics/1/unsubscribe", headers: headers
+      expect(response).to have_http_status(:success)
+      expect(data[:attributes]).to eq(user_topic1_attributes)
+      expect(UserTopic.first.subscribed).to eq(false)
     end
   end
 
