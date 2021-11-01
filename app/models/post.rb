875b6b5182afecc578db99504b19c88d28b6fcb7
jim <jbmbroomfield@gmail.com>
2021-11-01 11:50:37 +1100

    fixed topic updating

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,21 +3,23 @@ class Post < ApplicationRecord
 	belongs_to :user
 	belongs_to :topic
 
-	before_save do
-    if !self.id
-      self.tag = self.topic.posts.count.to_s
-      self.topic.subscribers.each do |subscriber|
-        notification = Notification.find_or_create_by(
-          user: subscriber,
-          category: 'replies',
-          object_id: self.topic.id
-        )
-        notification.number ||= 0
-        notification.number += 1
-        notification.save
-      end
-    end
-		ActionCable.server.broadcast("topic_#{self.topic.id}", type: 'update')
+	before_create do
+    self.tag = self.topic.posts.count.to_s
 	end
+
+  after_commit do
+		ActionCable.server.broadcast("topic_#{self.topic.id}", type: 'update')
+
+    self.topic.subscribers(self.user).each do |subscriber|
+      notification = Notification.find_or_create_by(
+        user: subscriber,
+        category: 'replies',
+        object_id: self.topic.id
+      )
+      notification.number ||= 0
+      notification.number += 1
+      notification.save
+    end
+  end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,8 +6,11 @@ class Topic < ApplicationRecord
 
 	after_save :broadcast_update
 
-  def subscribers
-    self.user_topics.where(subscribed: true).map{ |user_topic| user_topic.user }
+  def subscribers(excluding=nil)
+    self.user_topics
+    .where(subscribed: true)
+    .where.not(user: excluding)
+    .map{ |user_topic| user_topic.user }
   end
 
 end
