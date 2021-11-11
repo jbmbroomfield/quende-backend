1ed9a433b9c9b238a04179645bd1744d1814d1fd
jim <jbmbroomfield@gmail.com>
2021-11-11 11:47:25 +1100

    Added subsection_update to subsection_channel

--- app/channels/subsection_channel.rb
+++ app/channels/subsection_channel.rb
@@ -4,12 +4,22 @@ class SubsectionChannel < ApplicationCable::Channel
         stream_from "subsection_#{params[:subsection_id]}"
     end
 
+    def self.broadcast(subsection_id, **params)
+        ActionCable.server.broadcast("subsection_#{subsection_id}", **params)
+    end
+
     def self.topic_update(topic)
-        subsection_id = topic.subsection.id
-        ActionCable.server.broadcast(
-            "subsection_#{subsection_id}",
+        self.broadcast(
+            topic.subsection.id,
             type: 'topic_update',
-            topic_id: topic.id
+            topic_id: topic.id,
+        )
+    end
+
+    def self.subsection_update(subsection)
+        self.broadcast(
+            subsection.id,
+            type: 'subsection_update',
         )
     end
 
