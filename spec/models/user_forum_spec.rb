af47e7de64155df5b3212d9d9ea7803004ac2961
jim <jbmbroomfield@gmail.com>
2022-01-30 14:14:03 +1100

    UserForum can be created

--- spec/models/user_forum_spec.rb
+++ spec/models/user_forum_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UserForum, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'can be created' do
+    user = User.create(username: 'Test User')
+    forum = Forum.create(title: 'Test Forum')
+    user_forum = UserForum.create(user: user, forum: forum)
+    expect(user_forum.save).to eq(true)
+  end
+  
 end
