868489da6b213006d1552f42a0d4422542eac604
jim <jbmbroomfield@gmail.com>
2022-01-30 14:05:13 +1100

    Subsection set slug from title

--- app/models/subsection.rb
+++ app/models/subsection.rb
@@ -1,27 +1,16 @@
 class Subsection < ApplicationRecord
+
   include LastPostHelper
+
+  include SlugHelper
 
   belongs_to :section
   has_many :topics
   has_many :posts, through: :topics
-    
+  
+  before_create :set_slug_from_title
+  
   after_save :broadcast_main_update
-
-  before_create do
-    initial_slug = title.gsub(/_/, '-').parameterize
-    slug = initial_slug
-    number = 1
-    loop do
-      subsections = Subsection.where(slug: slug)
-      if subsections.count > 0
-        number += 1
-        slug = initial_slug + "-#{number}"
-      else
-        break
-      end
-    end
-    self.slug = slug
-  end
 
   def topic_count
     topics.count
