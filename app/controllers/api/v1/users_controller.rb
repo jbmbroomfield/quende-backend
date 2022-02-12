653844019a12b243e3a6a9f38ddfcfea0585e587
jim <jbmbroomfield@gmail.com>
2022-02-12 14:51:43 +1100

    User create now returns jwt with response body

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::UsersController < ApplicationController
 	def create
 		user = User.create_member(user_params)
 		if user.valid?
-			render json: UserSerializer.new(user), status: :created
+			token = encode_token({ user_id: user.id })
+			render json: { user: UserSerializer.new(user), jwt: token }, status: :created
 		else
 			render_user_not_created(user)
 		end
